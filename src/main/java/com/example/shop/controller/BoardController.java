432e59dc5e22245018d9f471b0c250f2cf18ef32
hyeok-min <gurals04@gmail.com>
2023-05-10 11:44:40 +0900

    문의로 가기

--- src/main/java/com/example/shop/controller/BoardController.java
+++ src/main/java/com/example/shop/controller/BoardController.java
@@ -5,11 +5,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
 import lombok.extern.slf4j.Slf4j;
@@ -22,10 +25,13 @@ public class BoardController {
 	  private String apiServer;
 	 
 	@GetMapping("/inquiry")
-	public String inquiry(Model model) throws Exception {
+	public String inquiry(Model model, @RequestParam("tokenname") String tokenname) throws Exception {
 		 String url = apiServer + "board/user/my-board";
-		 ResponseEntity response = new
-		 RestTemplate().getForEntity(url, Object.class);
+		HttpHeaders headers=new HttpHeaders();
+		headers.set("Authorization","Bearer "+tokenname);
+		HttpEntity<?> request = new HttpEntity<>(headers);
+
+		ResponseEntity response = new RestTemplate().exchange(url,HttpMethod.GET,request,Object.class);
 		  
 		 List boardList = (List) response.getBody();
 		 log.info("list = {}", boardList);
